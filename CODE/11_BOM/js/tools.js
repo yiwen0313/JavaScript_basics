f1f6f48aaed8afdfc3a36fd3f7b1a2fe1ff3907e
yiwen0313 <2469119402@qq.com>
2021-09-07 15:12:35 +0800

    类的操作 学习完成

--- CODE/11_BOM/js/tools.js
+++ CODE/11_BOM/js/tools.js
@@ -71,3 +71,71 @@ function getStyle(obj, name) {
     }
 
 }
+
+
+
+
+
+// 定义一个函数,用来向一个元素中添加指定的class属性值
+
+/* 
+ * 参数：
+ *   obj：要添加class属性的元素
+ *   cn： 要添加的class值
+ */
+function addClass(obj, cn) {
+
+    // 检查obj中是否含有cn
+    if (!hasClass(obj, cn)) {
+        obj.className += " " + cn;
+    }
+
+}
+
+/* 
+ * 判断一个函数中是否含有指定的class属性值
+ * 参数：
+ *    obj：要判断的元素
+ *    cn：class值
+ * 返回值：
+ *    有该class：返回true; 否则返回false
+ */
+function hasClass(obj, cn) {
+    // 判断obj中有没有cn class
+    // 创建一个正则表达式
+    // var reg = /\bb2\b/;
+    var reg = new RegExp("\\b" + cn + "\\b");
+
+    return reg.test(obj.className);
+}
+
+
+/* 
+ * 删除一个元素中指定的class属性
+ * 参数：
+ *    obj：要判断的元素
+ *    cn：class值
+ */
+function removeClass(obj, cn) {
+    // 创建一个正则表达式
+    var reg = new RegExp("\\b" + cn + "\\b");
+
+    // 删除class
+    obj.className = obj.className.replace(reg, " ")
+}
+
+/* 
+ * 切换一个类
+ *    - 如果元素中有该类，则删除
+ *    - 如果元素中无该类，则添加
+ */
+function toggleClass(obj, cn) {
+    // 判断obj中是否含有cn
+    if (hasClass(obj, cn)) {
+        // 有，删除
+        removeClass(obj, cn);
+    } else {
+        // 没有，添加
+        addClass(obj, cn);
+    }
+}
